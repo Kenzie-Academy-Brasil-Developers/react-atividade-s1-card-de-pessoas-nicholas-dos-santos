c309b08ee654629a42aa11ad3e0a95a51ebb49a9
nicholas <nicholassantos2017@gmail.com>
2021-10-28 05:54:10 -0300

    criando mais users

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import PersonCard from "./components/Developers.jsx";
 
 function App() {
-  const user = { Dev: "Nicholas", Idade: 23, Pais: "Brasil" };
+  const user1 = { Dev: "Nicholas", Idade: 23, Pais: "Brasil" };
+  const user2 = { Dev: "Paolo", Idade: 42, Pais: "Peru" };
+  const user3 = { Dev: "Amanda", Idade: 21, Pais: "Argentina" };
 
   return (
     <div className="App">
       <header className="App-header">
-        <PersonCard name={user.Dev} age={user.Idade} country={user.Pais} />
+        <PersonCard name={user1.Dev} age={user1.Idade} country={user1.Pais} />
+        <PersonCard name={user2.Dev} age={user2.Idade} country={user2.Pais} />
+        <PersonCard name={user3.Dev} age={user3.Idade} country={user3.Pais} />
       </header>
     </div>
   );
